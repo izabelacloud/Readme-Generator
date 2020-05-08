f8d47fc73ac1f43020b0900b857053521d27ff4c
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-05-08 12:01:07 -0700

    added all the questions

--- index.js
+++ index.js
@@ -10,6 +10,32 @@ const promptUser = () => {
     return inquirer.prompt([
         {
             type: "input",
+            name: "github",
+            message: "Enter your GitHub Username (Required)",
+            validate: GitHubUsernameInput => {
+                if(GitHubUsernameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: (Required)",
+            validate: emailInput => {
+                if(emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
             name: "title",
             message: "What is the title of your project? (Required)",
             validate: titleInput => {
@@ -50,23 +76,53 @@ const promptUser = () => {
         {
             type: "input",
             name: "usage",
-            message: "Please enter information about Usage: ",
+            message: "Please enter information about Usage: (Required)",
             validate: usageInput => {
                 if (usageInput) {
                     return true;
                 } else {
-                    console.log("Please enter Usage information");
+                    console.log("Please enter Usage details!");
                     return false;
                 }
             }
         },
-        // {
-        //     type: 
-        // }
-
-
-        
-
+        {
+            type: "input",
+            name: "contribution",
+            message: "Please enter Contribution details: (Required)",
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter Contribution details!");
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "test-instructions",
+            message: "Please enter Test Instructions: (Required)",
+            validate: testInstructionsInput => {
+                if (testInstructionsInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Please select from the list of licenses: (Required)",
+            choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"],
+            validate: licenseSelection => {
+                if (licenseSelection) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
 
     ])
    
